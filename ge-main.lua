d4c0f2720a4a174191e289edd3c857f6bb60df9b
Jed Northridge <git@jedcn.com>
2026-01-18 13:45:47 -0500

    Add some documentation for each alias

--- ge-main.lua
+++ ge-main.lua
@@ -10,11 +10,28 @@
 --
 echo("Starting to read ge-main.lua")
 
+--
+-- Make it so that..
+--
+--   scapl1 -> sca pl 1
+--   scapl2 -> sca pl 2
+--   scapl3 -> sca pl 3
+-- 
 createAlias("^scapl(\\d+)$", function(matches)
     local planetToScan = matches[2]
     send("sca pl " .. planetToScan)
 end, { type = "regex" })
 
+--
+-- Make it so that
+--
+-- setpl 1
+--
+-- Stores "1" as the planet you're tracking and later on,
+--
+-- scapl -> sca pl 1
+-- orbpl -> orb 1
+--
 local storedPlanet = 1
 createAlias("^setpl (\\d+)$", function(matches)
     storedPlanet = tonumber(matches[2])

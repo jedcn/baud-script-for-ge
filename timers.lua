d9892400b45097ea82487d327610c10b5cf7a20f
Jed Northridge <git@jedcn.com>
2026-02-06 12:20:43 -0500

    Not advancing retake-ilus at this time

--- timers.lua
+++ timers.lua
@@ -1,11 +1,3 @@
--- Retake Ilus tick (if function exists)
-if retakeIlusTick then
-    local status, err = pcall(retakeIlusTick)
-    if not status then
-        echo("\n\nCaught an error in retakeIlusTick:\n\n" .. err)
-    end
-end
-
 -- Navigation tick (if function exists)
 if navigationTick then
     local navStatus, navErr = pcall(navigationTick)
